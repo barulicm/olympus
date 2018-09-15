3771a352b3cfdf1811ddc6d64eb5a3f7fd6434ec
Mathew Barulic <matthew.barulic@gmail.com>
2018-09-15 00:42:23 -0400

    Adds command line support for loading sessions.

--- main.cpp
+++ main.cpp
@@ -22,24 +22,35 @@ vector<string> getDirectories(string root) {
 
 int main(int argc, char *argv[]) {
 
-    auto competitions = getDirectories("resources/dynamic");
-
-    cout << "Choose a competition:\n";
-    auto i = 0;
-    for(const auto &competition : competitions) {
-        cout << i << " - " << competition << "\n";
-        i++;
-    }
-    size_t competitionIndexIn;
-    cin >> competitionIndexIn;
-    competitionIndexIn = min(competitionIndexIn, competitions.size());
-
     auto url = U("http://127.0.0.1:8080");
 //    auto url = U("http://192.168.1.20:8080");
-
     uri_builder uri{url};
     auto address = uri.to_uri().to_string();
-    auto httpHandler = HTTPHandler::fromUrlAndCompetitionName(address, competitions[competitionIndexIn]);
+
+    std::unique_ptr<HTTPHandler> httpHandler;
+
+    cout << "New session? [(y)/n]:\n";
+    auto newSessionSelection = '\0';
+    cin >> newSessionSelection;
+    if(newSessionSelection == 'y') {
+        cout << "Choose a competition:\n";
+        auto competitions = getDirectories("resources/dynamic");
+        auto i = 0;
+        for(const auto &competition : competitions) {
+            cout << i << " - " << competition << "\n";
+            i++;
+        }
+        size_t competitionIndexIn;
+        cin >> competitionIndexIn;
+        competitionIndexIn = min(competitionIndexIn, competitions.size());
+        httpHandler = HTTPHandler::fromUrlAndCompetitionName(address, competitions[competitionIndexIn]);
+    } else {
+        cout << "Please provide a session file to load:\n";
+        std::string sessionFilePath;
+        cin >> sessionFilePath;
+        httpHandler = HTTPHandler::fromUrlAndSavedSession(address, sessionFilePath);
+    }
+
     httpHandler->open().wait();
 
     cout << "Listening for requests at: " << address << endl;
