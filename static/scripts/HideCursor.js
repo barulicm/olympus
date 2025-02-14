bd9e1dfe7cd60fcf1002e5ab141b0cc0a2552c6e
Matthew Barulic <matthew.barulic@gmail.com>
2025-02-14 00:28:08 -0500

    Fixes null error on Display page load

--- static/scripts/HideCursor.js
+++ static/scripts/HideCursor.js
@@ -2,12 +2,16 @@ let cursorHideTimeoutId;
 const cursorHideDelay = 1000;  // ms
 
 function hideCursor() {
-    document.body.style.cursor = 'none';
+    if(document.body !== null) {
+        document.body.style.cursor = 'none';
+    }
 }
 
 function resetInactivityTimer() {
     clearTimeout(cursorHideTimeoutId);
-    document.body.style.cursor = 'default';
+    if(document.body !== null) {
+        document.body.style.cursor = 'default';
+    }
     cursorHideTimeoutId = setTimeout(hideCursor, cursorHideDelay);
 }
 
