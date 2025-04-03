e0bdc50c1726737cda347ff1c8b6135c47153dac
Matthew Barulic <matthew.barulic@gmail.com>
2025-04-02 23:42:56 -0400

    Adds tests to AnnouncementHandler.

--- src/handlers/announcement_handler.rs
+++ src/handlers/announcement_handler.rs
@@ -55,3 +55,104 @@ impl AnnouncementHandler {
         Ok(StatusCode::OK)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::app_state::create_new_shared_state;
+    use axum::body::Body;
+    use axum::http::{Request, StatusCode, header};
+    use http_body_util::BodyExt;
+    use serde_json::{Value, json};
+    use tower::{Service, ServiceExt};
+
+    #[tokio::test]
+    async fn defaults_to_no_announcement() {
+        let app_state = create_new_shared_state();
+        let app = AnnouncementHandler::register_routes().with_state(app_state);
+
+        let req = Request::builder()
+            .method("GET")
+            .uri("/announcement")
+            .body(Body::empty())
+            .unwrap();
+        let response = app.oneshot(req).await.unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({ "content": "", "visible": false }));
+    }
+
+    #[tokio::test]
+    async fn setting_announcement_works() {
+        let app_state = create_new_shared_state();
+        let mut app = AnnouncementHandler::register_routes()
+            .with_state(app_state)
+            .into_service();
+
+        let req = Request::builder()
+            .method("PUT")
+            .uri("/announcement")
+            .header(header::CONTENT_TYPE, "application/json")
+            .body(Body::from(
+                r#"{"content": "Test announcement", "visible": true}"#,
+            ))
+            .unwrap();
+        let response = ServiceExt::<Request<Body>>::ready(&mut app)
+            .await
+            .unwrap()
+            .call(req)
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let req = Request::builder()
+            .method("GET")
+            .uri("/announcement")
+            .body(Body::empty())
+            .unwrap();
+        let response = ServiceExt::<Request<Body>>::ready(&mut app)
+            .await
+            .unwrap()
+            .call(req)
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(
+            body,
+            json!({ "content": "Test announcement", "visible": true })
+        );
+
+        let req = Request::builder()
+            .method("PUT")
+            .uri("/announcement")
+            .header(header::CONTENT_TYPE, "application/json")
+            .body(Body::from(r#"{"content": "", "visible": false}"#))
+            .unwrap();
+        let response = ServiceExt::<Request<Body>>::ready(&mut app)
+            .await
+            .unwrap()
+            .call(req)
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let req = Request::builder()
+            .method("GET")
+            .uri("/announcement")
+            .body(Body::empty())
+            .unwrap();
+        let response = ServiceExt::<Request<Body>>::ready(&mut app)
+            .await
+            .unwrap()
+            .call(req)
+            .await
+            .unwrap();
+        assert_eq!(response.status(), StatusCode::OK);
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body).unwrap();
+        assert_eq!(body, json!({ "content": "", "visible": false }));
+    }
+}
